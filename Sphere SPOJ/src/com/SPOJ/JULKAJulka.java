726663de2008e3f99f4fd872c069411a097acd35
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2020-06-29 10:41:28 +0600

    No commit, just add

---
+++ Sphere SPOJ/src/com/SPOJ/JULKAJulka.java
@@ -0,0 +1,24 @@
+package com.SPOJ;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class JULKAJulka
+{
+    public static void main(String[] args)
+    {
+        int loop = 10;
+        while (loop > 0)
+
+        {
+            BigInteger togather, differ, firstOne, secondOne;
+            firstOne = BigInteger.ZERO;
+            secondOne = BigInteger.ZERO;
+            Scanner input = new Scanner(System.in);
+            togather = input.nextBigInteger();
+            differ = input.nextBigInteger();
+
+            loop--;
+        }
+    }
+}
